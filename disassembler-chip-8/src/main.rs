34ca7072010d39e38ac15a7fe5e5797898a14305
shh4und <alexandersznunes@gmail.com>
2025-10-03 00:41:25 -0300

    calls disassembler

--- disassembler-chip-8/src/main.rs
+++ disassembler-chip-8/src/main.rs
@@ -1,8 +1,21 @@
 mod utils;
-
+mod disassembler;
+use std::env;
+use std::fs;
+// use std::io;
 
 fn main() {
-    let filepath = "../programs/skyward.ch8";
-    
-    
+    let args: Vec<String> = env::args().collect();    
+    if args.len() < 2 {
+        eprintln!("Use: cargo run -- <file.ch8>");
+        
+        return;
+    }
+
+    let filepath = &args[1];
+    let rom = fs::read(filepath).expect("Error at handling file {filepath}");
+    println!("Disassembling {} ({} bytes)", filepath, rom.len());
+
+    disassembler::run(rom);
+
 }
