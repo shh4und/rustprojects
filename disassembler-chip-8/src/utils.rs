9623085e39fd3111b7601866de76e123affc85c5
shh4und <alexandersznunes@gmail.com>
2025-10-03 00:40:56 -0300

    extract byte fields

--- disassembler-chip-8/src/utils.rs
+++ disassembler-chip-8/src/utils.rs
@@ -1,8 +1,16 @@
-use std::fs;
-use std::io;
 
-pub fn read_file_bytes(filepath: &str) -> io::Result<Vec<u8>> {
-    fs::read(filepath)
+pub fn nnn(opcode: u16) -> u16 {
+    opcode & 0x0FFF
 }
 
+pub fn x(opcode: u16) -> u8 {
+    ((opcode & 0x0F00) >> 8) as u8
+}
 
+pub fn y(opcode: u16) -> u8 {
+    ((opcode & 0x00F0) >> 4) as u8
+}
+
+pub fn kk(opcode: u16) -> u8 {
+    (opcode & 0x00FF) as u8
+}
